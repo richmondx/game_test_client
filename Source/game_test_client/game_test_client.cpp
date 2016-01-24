1ba2341031e0ec8904b400f18bcc58d726da36e1
alezrik@bitbucket.com <ex_ra@hotmail.com>
2016-01-24 02:46:41 -0800

    initial add of game client

---
+++ Source/game_test_client/game_test_client.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "game_test_client.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, game_test_client, "game_test_client" );
---
+++ Source/game_test_client/game_test_clientGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "game_test_clientGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_TEST_CLIENT_API Agame_test_clientGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
