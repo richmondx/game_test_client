8cbe91b568c62cec404b827835444bac32405230
alezrik@bitbucket.com <ex_ra@hotmail.com>
2016-01-28 02:29:37 -0800

    test client modes, etc

---
+++ Source/game_test_client/EchoTestGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "game_test_client.h"
+#include "EchoTestGameMode.h"
+
+
+
+
---
+++ Source/game_test_client/EchoTestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "EchoTestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_TEST_CLIENT_API AEchoTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
